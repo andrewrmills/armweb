cb0e50bfa401f8735a0f8d9872cbf013b6b40bae
andrewrmills <andrewmills2515@gmail.com>
2022-05-03 17:49:04 +1200

    resized images

--- script/script.js
+++ script/script.js
@@ -35,19 +35,6 @@ function revealRight() {
 
 window.addEventListener("scroll", revealRight);
 
-// $(window).scroll(function() {
-//     $('.fade-in').each(function() {
-//         var top_of_element = $(this).offset().top;
-//         var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-//         var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
-//         var top_of_screen = $(window).scrollTop();
-
-//         if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element) && !$(this).hasClass('is-visible')) {
-//             $(this).addClass('is-visible');
-//         }
-//     });
-// });
-
 // Stars
 const STAR_COLOR = '#fff';
 const STAR_SIZE = 3;
